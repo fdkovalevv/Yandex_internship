624a3ad3a6a1720967f66f77b34253ce81746498
fdkovalevv <114774497+fdkovalevv@users.noreply.github.com>
2024-11-01 00:28:28 +0300

    Update Training_6:Task3.py
    Дописал решение 3 задачи из тренировок от яндекса, оно оказалось не таким сложным

--- ALG_1/Training_6:Task3.py
+++ ALG_1/Training_6:Task3.py
@@ -1,77 +1,56 @@
-def check_I(board, n):
-    for j in range(n):
-        if board[0][j] != '#' or board[n-1][j] != '#':
-            return False
-    for i in range(n):
-        if board[i][0] != '#' or board[i][n-1] != '#':
-            return False
-    return True
+def compress(lst):
+    ans = [lst[0]]
+    for now in lst[1:]:
+        if now != ans[-1]:
+            ans.append(now)
+    if len(ans) > 1 and set(ans[0]) == {'.'}:
+        ans.pop(0)
+    if len(ans) > 1 and set(ans[-1]) == {'.'}:
+        ans.pop()
+    return ans
 
-def check_O(board, n):
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == '#':
-                value = ((0 < j < n-1) and (0 < i < n-1))
-                if value:
-                    internal = board[i-1][j-1] == board[i+1][j+1] == '.' and \
-                               board[i-1][j+1] == board[i+1][j-1] == '.'
-                    if internal:
-                        return True
-    return False
+def inverse(lst):
+    ans = []
+    for i in range(len(lst[0])):
+        ans.append([])
+    for now in lst:
+        for i in range(len(now)):
+            ans[i].append(now[i])
+    for i in range(len(ans)):
+        ans[i] = ''.join(ans[i])
+    return ans
 
-def check_C(board, n):
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == '#':
-                # Check if the right border is at the edge.
-                if j < n - 1 and board[i][n-1] == '#':
-                    return True
-    return False
+n = int(input())
+a = []
+for i in range(n):
+    a.append(input())
 
-def check_L(board, n):
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == '#':
-                if i < n - 1 and j < n - 1 and j == n - 1:
-                    return True
-    return False
-
-def check_H(board, n):
-    # check for H structure
-    for j in range(n):
-        if board[0][j] == '#' and board[n-1][j] == '#':
-            if j > 0 and board[n//2][j] == '#':
-                return True
-    return False
-
-def check_P(board, n):
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == '#':
-                if (i < n-1 and j < n-1 and board[i][n-1] == '#' and
-                    board[n-1][j] == '#'):
-                    return True
-    return False
-
-# Основная функция
-def identify_letter(n, board):
-    if check_I(board, n):
-        return 'I'
-    if check_O(board, n):
-        return 'O'
-    if check_C(board, n):
-        return 'C'
-    if check_L(board, n):
-        return 'L'
-    if check_H(board, n):
-        return 'H'
-    if check_P(board, n):
-        return 'P'
-    return 'X'
-
-# Чтение входных данных
-n = int(input().strip())
-board = [input().strip() for _ in range(n)]
-
-# Вывод результата
-print(identify_letter(n, board))
+a = compress(a)
+a = inverse(a)
+a = compress(a)
+a = inverse(a)
+if a == ['#']:
+    print('I')
+elif a == ['###',
+           '#.#',
+           '###']:
+    print('O')
+elif a == ['##',
+           '#..',
+           '##']:
+    print('C')
+elif a == [
+           '#.',
+           '##']:
+    print('L')
+elif a == ['#.#',
+           '###',
+           '#.#']:
+    print('H')
+elif a == ['###',
+           '#.#',
+           '###',
+           '#..']:
+    print('P')
+else:
+    print('X')
